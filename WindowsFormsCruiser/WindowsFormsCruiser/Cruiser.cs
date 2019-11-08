05245849eb3e7ed2931ad5a8b0029dae54c41920
helencruel77 <helencruel228@gmail.com>
2019-11-08 14:09:41 +0300

    laba1

---
+++ WindowsFormsCruiser/WindowsFormsCruiser/Cruiser.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsCruiser;
+
+namespace WindowsFormscruiser
+{
+    class Cruiser
+    {
+        
+        private float _startPosX;
+       
+        private float _startPosY;
+      
+        private int _pictureWidth;
+        
+        private int _pictureHeight;
+       
+        private const int carWidth = 100;
+        
+        private const int carHeight = 60;
+        
+        public int MaxSpeed { private set; get; }
+        
+        public float Weight { private set; get; }
+        
+        public Color MainColor { private set; get; }
+        
+        public Color DopColor { private set; get; }
+       
+        public bool Flag { private set; get; }
+       
+        public bool Rocket { private set; get; }
+
+        public Cruiser(int maxSpeed, float weight, Color mainColor, Color dopColor,
+       bool flag, bool rocket)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            Flag = flag;
+            Rocket = rocket;
+        }
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+        public void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - carWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+               
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - carHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+        public void DrawCruiser(Graphics g)
+        {
+            Brush main = new SolidBrush(MainColor);
+            Brush dop = new SolidBrush(DopColor);
+
+            Brush red = new SolidBrush(Color.Red);
+            Brush blue = new SolidBrush(Color.Blue);
+            Brush white = new SolidBrush(Color.White);
+            g.FillRectangle(main, _startPosX, _startPosY + 30, 90, 15);
+            g.FillRectangle(dop, _startPosX + 20, _startPosY + 20, 55, 10);
+            if (Flag)
+            {
+                g.FillRectangle(main, _startPosX + 35, _startPosY, 8, 20);
+                g.FillRectangle(white, _startPosX + 35, _startPosY + 33, 18, 3);
+                g.FillRectangle(blue, _startPosX + 35, _startPosY + 36, 18, 3);
+                g.FillRectangle(red, _startPosX + 35, _startPosY + 39, 18, 3);
+            }
+
+            if (Rocket)
+            {
+
+                g.FillRectangle(dop, _startPosX + 30, _startPosY + 45, 3, 10);
+
+                g.FillEllipse(dop,_startPosX + 15, _startPosY + 53, 35, 5);
+                g.FillEllipse(dop,_startPosX + 45, _startPosY + 53, 5, 5);
+           
+                g.FillRectangle(red,_startPosX + 45, _startPosY + 54, 3, 4);
+
+            }
+           
+        }
+    }
+}
+
---
+++ WindowsFormsCruiser/WindowsFormsCruiser/FormCruiser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsCruiser;
+
+namespace WindowsFormscruiser
+{
+    public partial class FormCruiser : Form
+    {
+        private Cruiser cruiser;
+
+        public FormCruiser()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxCruiser.Width, pictureBoxCruiser.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+
+            cruiser.DrawCruiser(gr);
+            pictureBoxCruiser.Image = bmp;
+        }
+        
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            cruiser = new Cruiser(rnd.Next(300, 600), rnd.Next(1000, 1000), Color.Black,
+           Color.Gray, true, true);
+            cruiser.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxCruiser.Width,
+           pictureBoxCruiser.Height);
+            Draw();
+        }
+      
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    cruiser.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    cruiser.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    cruiser.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    cruiser.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+    }
+}
